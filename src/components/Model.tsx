8b1762e52f6bf99c7bbd8ba15a2d57991d075133
FedoRich-P <panchev_fedor@mail.ru>
2024-10-21 19:40:40 +0300

    first commit

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {adidasArr} from "./pages/Adidas";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 export const Model = () => {
 
@@ -11,6 +11,12 @@ export const Model = () => {
             <h2> ADIDAS</h2>
             {params.id &&
                 <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem'}}>
+                    <Link to="/adidas">
+                        <button>Back to Adidas</button>
+                    </Link>
+                    <Link to="/">
+                        <button>Back Home</button>
+                    </Link>
                     <img style={{width: '300px', height: '300px'}} src={adidasArr[+params.id].picture} alt={adidasArr[+params.id].model}/>
                     <h3>{adidasArr[+params.id].model}</h3>
                     <h4>{adidasArr[+params.id].price}</h4>
